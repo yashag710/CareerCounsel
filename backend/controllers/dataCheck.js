7887212da9f88d67f659d1dcb32d05eb37044750
yashag710 <yashag1810@gmail.com>
2025-03-16 14:30:58 +0530

    Data Check

---
+++ backend/controllers/dataCheck.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const userModel = require("../models/userModel");
+
+exports.dataCheck = async function (req, res) {
+  try {
+    const user = await userModel.findOne({ email: req.body.email }).select("skills");
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found!" });
+    }
+
+    if (user.skills && user.skills.length > 0) {
+      return res.redirect(`/userdash?email=${user.email}`); 
+    } else {
+      return res.redirect("/careerform");
+    }
+  } catch (err) {
+    console.error("Error in dataCheck:", err.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
