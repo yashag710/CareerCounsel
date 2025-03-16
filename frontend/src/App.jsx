aef236ebaa393e2981c9af4abd77e711cb58b943
yashag710 <yashag1810@gmail.com>
2025-03-16 12:05:41 +0530

    Ten Commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import UserDash from "./components/UserDash";
+import { BrowserRouter as Router , Routes , Route } from "react-router-dom";
 import "./App.css";
+import { Home } from "./components/Home";
+import Login from "./components/Login";
 function App(){
   return(
-    <>
-    <UserDash />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/userdash" element={<UserDash />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   )
 }
 
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -11,5 +11,3 @@ root.render(
   </StrictMode>,
 );
 
-
-reportWebVitals();
