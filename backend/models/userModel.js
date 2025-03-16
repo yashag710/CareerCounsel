34b358efc5869b3d8ad925accfe313933b571a6d
yashag710 <yashag1810@gmail.com>
2025-03-16 11:07:48 +0530

    Token setup

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,9 +1,29 @@
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-    fullname : String,
-    email : String,
-    password : String
+    fullname : {
+        type : String,
+        required : true
+    },
+    email : {
+        type : String,
+        required : true
+    },
+    password : {
+        type : String,
+        required : true
+    },
+    skills : {
+        type : [String],
+        default : [""],
+        required : true
+    },
+    city : String,
+    country : String,
+    experience : {
+        type : Number,
+        required : false
+    }
 });
 
 module.exports = mongoose.model("user" , userSchema);
